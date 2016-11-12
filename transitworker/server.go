9d91f5aac815801168530c283ea521dd47712ddb
Chris Hoffman <christopher.hoffman@gmail.com>
2016-11-11 21:22:17 -0500

    server constructor

--- transitworker/server.go
+++ transitworker/server.go
@@ -1,6 +1,17 @@
 package transitworker
 
 type Server struct {
+	config *Config
+
 	// vault is the client for communicating with Vault.
 	vault vaultClient
+}
+
+func NewServier(config *Config) (*Server, error) {
+	// Create the server
+	s := &Server{
+		config: config,
+	}
+
+	return s, nil
 }
